4cc56d56909a86b50e293d5ad153155493fc8851
Preeteesh Sharma <prtshshrm@gmail.com>
2018-11-23 16:16:26 +0530

    Finish Static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
+  
+	root 'static_pages#home'
   get 'static_pages/home'
 
   get 'static_pages/help'
+  #manually adding an about page 
+  #hence tell rails to route a get request for /about url
+  get  'static_pages/about'
+
+  get  'static_pages/contact'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#hello'
 end
