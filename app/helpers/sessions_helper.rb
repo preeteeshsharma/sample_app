f26880f9388acfe27da05479400a2f556d83a9d2
Preeteesh Sharma <prtshshrm@gmail.com>
2019-01-15 17:40:25 +0530

    Implement advanced login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,19 +1,51 @@
 module SessionsHelper
 	def log_in(user)
     	session[:user_id] = user.id
-  	end
+  end
 
+  # Returns the current logged-in user (if any).
 	def current_user
     	if session[:user_id]
       	@current_user ||= User.find_by(id: session[:user_id])
     	end
-  	end
+	end
 
 	def logged_in?
     	!current_user.nil?
-  	end  	
+  end  
+
+  # Remembers a user in a persistent session.
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end	
+
+  # Forgets a persistent session.
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
+  # Returns the user corresponding to the remember token cookie.
+  def current_user
+    #if session user id exists assign it to user id.
+    #the meaning of an existing session id is that user hasn't closed the browser
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    # else if a cookie exists, then assign it to the user_id
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
+  end
 
   def log_out
+    forget(current_user)
     session.delete(:user_id)
     @current_user = nil
   end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,7 +7,28 @@ class ActiveSupport::TestCase
   fixtures :all
  include ApplicationHelper
   # Add more helper methods to be used by all tests here...
+  
+  # Returns true if a test user is logged in.
+  #Inside controller tests, we can manipulate the session method directly, assigning user.id to the :user_id key 
   def is_logged_in?
     !session[:user_id].nil?
+  end
+
+  # Log in as a particular user.
+  def log_in_as(user)
+    session[:user_id] = user.id
+  end
+end
+
+#Inside integration tests, we can’t manipulate session directly, but we can post to the sessions path
+class ActionDispatch::IntegrationTest
+
+  # Log in as a particular user.
+  #because it’s located inside the ActionDispatch::IntegrationTest class, 
+  #this is the version of log_in_as that will be called inside integration tests
+  def log_in_as(user, password: 'password', remember_me: '1')
+    post login_path, params: { session: { email: user.email,
+                                          password: password,
+                                          remember_me: remember_me } }
   end
 end
